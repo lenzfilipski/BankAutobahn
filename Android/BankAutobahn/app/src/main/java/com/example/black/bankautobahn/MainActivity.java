3b37825a1472581044e81a323f8e0e07cd1a1e8d
Blackoutburst <blackoutburst@FChanson>
2019-02-20 15:31:30 +0100

    App Finished

--- Android/BankAutobahn/app/src/main/java/com/example/black/bankautobahn/MainActivity.java
+++ Android/BankAutobahn/app/src/main/java/com/example/black/bankautobahn/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.black.bankautobahn;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://lenz.filipski.fr:8080/html/index.html");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
     }
 }
