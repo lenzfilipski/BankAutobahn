26ec302818ba9b5101a4ef7a5af513eafdafcacd
Blackoutburst <blackoutburst@hotmail.com>
2019-03-13 09:37:07 +0100

    Updating app code

--- Android/BankAutobahn/app/src/main/java/com/example/black/bankautobahn/MainActivity.java
+++ Android/BankAutobahn/app/src/main/java/com/example/black/bankautobahn/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.black.bankautobahn;
 
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    WebView myWebView;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState )
+    {
+        super.onSaveInstanceState(outState);
+        myWebView.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        myWebView.restoreState(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +40,13 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView = (WebView) findViewById(R.id.webview);
         myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("http://lenz.filipski.fr:8080/html/index.html");
+        if (savedInstanceState == null) {
+            myWebView.loadUrl("http://lenz.filipski.fr:8080/html/index.php");
+        }
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
 
     }
 }
