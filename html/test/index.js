653ba2f179038e7a0baba8b65b6c8ce070eca5c4
Elyox <lenz.filipski@gmail.com>
2019-04-11 23:08:10 +0200

    Now ws test realy works

--- html/test/index.js
+++ html/test/index.js
@@ -2,22 +2,23 @@ var server = require('ws').Server;
 var serv1 = new server({ port: 5001 });
 
 serv1.on('connection', function(ws) {
-  console.log('conected');
+  console.log('connected');
   ws.on('message', function(message_str) {
-    console.log('Recieved: ' + message_str);
     var message = JSON.parse(message_str);
+    console.log('Recieved: ' + message.id + ' || ' + message.content);
 
     switch (message.id) {
       case 'test':
         if (message.content == 'hello') {
-          ws.send("{ 'id':'test', 'content':'hi from other world!'}");
+          ws.send(JSON.stringify({ "id":"test", "content":"hi from other world!"}));
         }
         else {
-          ws.send(message);
+          ws.send(message_str);
         }
         break;
       default:
         ws.send(JSON.stringify(message));
+        break;
     };
   });
 });
--- html/test/pingscript.js
+++ html/test/pingscript.js
@@ -1,18 +1,19 @@
 var sock = new WebSocket('ws://bank.filipski.fr:5001');
 sock.onopen = function (event) {
+  // Create a random session ID
   console.log('Connected!');
-  sock.send('{ "test":"text", "content":"Hello world!"}');
+  sock.send('{ "id":"test", "content":"Hello world!"}');
 };
 sock.onmessage = function (event) {
   console.log(event.data);
-  var in_mess = JSON.parse(event.data);
+  var message = JSON.parse(event.data);
 
   switch (message.id) {
     case 'ping':
       sock.send('{ "id":"ping", "content":"1" }')
       break;
     case 'test':
-      document.getElementById('enter1').innerHTML = in_mess.content;
+      document.getElementById('enter1').innerHTML = message.content;
       break;
   }
 };
