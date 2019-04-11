17bb62681a0433f2b1159212b818ab01cb7a5686
Elyox <lenz.filipski@gmail.com>
2019-04-11 22:38:40 +0200

    Correct some errors in ws test

--- html/test/index.js
+++ html/test/index.js
@@ -2,26 +2,22 @@ var server = require('ws').Server;
 var serv1 = new server({ port: 5001 });
 
 serv1.on('connection', function(ws) {
+  console.log('conected');
   ws.on('message', function(message_str) {
     console.log('Recieved: ' + message_str);
     var message = JSON.parse(message_str);
 
-    switch (message.type) {
-      case 'text':
-        switch (message.content) {
-          case 'hello':
-            ws.send("{ 'type':'text', 'content':'hi from other world!'}");
-            break;
+    switch (message.id) {
+      case 'test':
+        if (message.content == 'hello') {
+          ws.send("{ 'id':'test', 'content':'hi from other world!'}");
         }
+        else {
+          ws.send(message);
+        }
+        break;
       default:
         ws.send(JSON.stringify(message));
-    }
-
-    if (message == 'hello') {
-      ws.send('hi from other world!')
-    }
-    else {
-      ws.send(message)
-    }
+    };
   });
 });
--- html/test/pingscript.js
+++ html/test/pingscript.js
@@ -1,15 +1,18 @@
 var sock = new WebSocket('ws://bank.filipski.fr:5001');
 sock.onopen = function (event) {
   console.log('Connected!');
-  sock.send('{ "type":"text", "content":"Hello world!"}');
+  sock.send('{ "test":"text", "content":"Hello world!"}');
 };
 sock.onmessage = function (event) {
   console.log(event.data);
   var in_mess = JSON.parse(event.data);
-  switch (message.type) {
+
+  switch (message.id) {
     case 'ping':
       sock.send('{ "id":"ping", "content":"1" }')
-  };
-  document.getElementById('enter1').innerHTML = in_mess.content
-
+      break;
+    case 'test':
+      document.getElementById('enter1').innerHTML = in_mess.content;
+      break;
+  }
 };
