f1a9a466a31488141307485ae0db1c276039a347
Elyox <lenz.filipski@gmail.com>
2019-04-21 17:53:39 +0200

    ws -> wss test

--- html/test/pingscript.js
+++ html/test/pingscript.js
@@ -1,19 +1,26 @@
 // javascript du client / page web
+// console.log(x) permet d'afficher x dans la console
+// a.send(x) permet d'envoyer d'envoyer la chaine x avec la connection a
 
-// Cree un lien websocket avec le serveur
-var sock = new WebSocket('ws://lenz.filipski.fr:5001');
+// Cree un lien websocket secure avec le serveur
+var sock = new WebSocket('wss://lenz.filipski.fr:5001');
 
-// Execute a l'ouverture de la connection
+// Execute a l'ouverture de la connection avec le serveur
 sock.onopen = function (event) {
   console.log('Connected!');
   sock.send('testHello world!');
 };
+
+// Gere la réception des messages du serveur
 sock.onmessage = function (event) {
   console.log(event.data);
+
+  // Permet de récupérer et de séparer l'ID et le contenu recu du serveur
   var message = event.data;
   var id = message.slice(0, 4);
   var content = message.slice(4);
 
+  // Permet d'effectuer differentes actions en fonction de l'ID de la requette
   switch (id) {
     case 'ping':
       switch (content) {
@@ -24,6 +31,8 @@ sock.onmessage = function (event) {
 
       }
       break;
+
+    //
     case 'test':
       document.getElementById('enter1').innerHTML = content;
       break;
