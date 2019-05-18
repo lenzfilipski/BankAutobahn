e0f97f22aa86863daaad947ad699675e9cbda1a9
Elyox <lenz.filipski@gmail.com>
2019-05-18 22:58:34 +0200

    moved script into testping.php

--- html/test/testping.php
+++ html/test/testping.php
@@ -13,10 +13,53 @@ $_SESSION['loged'] = 'yes'?>
     <title> Test ping JS</title>
   </head>
   <body>
-    <script type="text/javascript"></script>
     <!-- importe pingscript.js -->
-    <script type="text/javascript" src="pingscript_test.js"></script>
+    <!-- <script type="text/javascript" src="pingscript_test.js"></script> -->
+    <script type="text/javascript">
+      // javascript du client / page web
+      // console.log(x) permet d'afficher x dans la console
+      // a.send(x) permet d'envoyer d'envoyer la chaine x avec la connection a
 
+      // Cree un lien websocket secure avec le serveur
+      // j'ai remplace le port par '/myws' et mis en place un reverse proxy sur le seveur
+      var sock = new WebSocket('wss://bank.filipski.fr/myws');
+
+      // Execute a l'ouverture de la connection avec le serveur ws
+      sock.onopen = function (event) {
+        console.log('Connected!');
+        sock.send('testHello world!');
+        var acc_id = <?php echo json_encode($_POST['inputId']); ?>;
+        var acc_pw = <?php echo json_encode($_POST['inputPassword']); ?>;
+        if (typeof acc_id != 'undefined' && typeof acc_pw != 'undefined') {
+          sock.send('cote'+acc_id+acc_pw)
+        };
+      };
+
+      // Gere la reception des messages du serveur ws
+      sock.onmessage = function (event) {
+        console.log(event.data);
+
+        // Permet de récupérer et de séparer l'ID et le contenu recu du serveur ws
+        var message = event.data;
+        var id = message.slice(0, 4);
+        var content = message.slice(4);
+
+        // Permet d'effectuer differentes actions en fonction de l'ID de la requette
+        switch (id) {
+          case 'conn':
+            if (content != '1') {
+              window.location.replace('../connection.php');
+            };
+            break;
+
+          // si id == test
+          case 'test':
+            // affiche le contenu de content dans la balise <output id="enter1">
+            document.getElementById('enter1').innerHTML = content;
+            break;
+        };
+      };
+    </script>
     <!-- Champ de texte a envoyer au serveur ws -->
     <input type=text onchange="var tempmes = 'test' + this.value; sock.send(tempmes);">
     <br>
