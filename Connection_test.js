3b11ace2fb31bf0ec3a6b3b5d6e57fbc15e13e98
Florian Chanson <30992311+Blackoutburst@users.noreply.github.com>
2019-05-19 18:19:50 +0200

    Mise a jours du fichier
    Exemple de code pour créer un compte et récupérer les information de connection

--- Connection_test.js
+++ Connection_test.js
@@ -11,3 +11,22 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+
+//Récupère compte et mot de passe
+con.query('SELECT account, password FROM users', function(err, rows, fields) {
+  if (!err)
+    console.log('account number: ', rows[0].account,' account password: ', rows[0].password);
+  else
+    console.log('Error while performing Query.');
+  });
+
+  //Créer un compte
+  con.query('INSERT INTO users (account, password) VALUES (0000000002, 4567)', function(err, rows, fields) {
+    if (!err)
+      console.log('Account created!');
+    else
+      console.log('Error while performing Query.');
+    });
+
+con.end();
