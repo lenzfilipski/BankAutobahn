1153e8f46ae5bb27236a06fcdc28c58885319946
Lenz Filipski <lenz.filipski@gmail.com>
2019-05-20 10:29:45 +0200

    lvzejruf

--- html/espacperso.php
+++ html/espacperso.php
@@ -71,6 +71,7 @@
           case 'vico':
             if (content == 'ok') {
               document.getElementById('virInfo').innerHTML = 'Votre virement à bien été effectué.';
+              virement();
             } else {
               document.getElementById('virInfo').innerHTML = 'Un erreur est survenue. Vérifiez votre solde ou le desitnataire.'
             }
