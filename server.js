a5c75a88361e14b7ae7a879b39d98ebf919d6d28
Florian Chanson <30992311+Blackoutburst@users.noreply.github.com>
2019-05-21 18:50:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const server = https.createServer({
   key: fs.readFileSync('/etc/letsencrypt/live/ws.bank.filipski.fr/privkey.pem')
 });
 
+var conect = false;
+var identifiant = "";
 var values = [[]];
 var mysql = require('mysql');
 var WebSocket = require('ws'); // Import ws
@@ -14,12 +16,11 @@ var serv1 = new WebSocket.Server({ server }); // Met en place le nouveau serveur
 serv1.on('connection', function(ws) {
   console.log('connected');
   ws.on('message', function(message_str) {
-    var conect = false;
-    var identifiant = "";
     var message = message_str;
     var id = message.slice(0, 4);
     var content = message.slice(4);
     console.log('Recieved: ' + id + ' || ' + content);
+    console.log("AAAAAAAAA "+conect);
 
 
     if (!conect) { // If not already connected
@@ -29,6 +30,7 @@ serv1.on('connection', function(ws) {
             var iden = content.slice(0, 10);
             var pw = content.slice(10);
             var con = mysql.createConnection({
+              multipleStatements: true,
               host: 'localhost',
               user: 'root',
               password: '1234',
@@ -44,6 +46,7 @@ serv1.on('connection', function(ws) {
                 if (pw == rows[0].password){
                   ws.send("coreok");
                   conect = true;
+                  console.log("BBBBBBBBB "+conect);
                   identifiant = iden;
                 }
                 else
@@ -59,6 +62,7 @@ serv1.on('connection', function(ws) {
             var iden = content.slice(0, 10);
             var pw = content.slice(10);
             var con = mysql.createConnection({
+              multipleStatements: true,
               host: 'localhost',
               user: 'root',
               password: '1234',
@@ -85,8 +89,31 @@ serv1.on('connection', function(ws) {
 
         case 'reqi': // create a new account
 
-            var iden = 1234567890;
-            ws.send("newi" + iden);
+            var maxidi = 9999999999;
+            var lastidi = 0;
+            var newidi = 0;
+
+            var con = mysql.createConnection({
+              multipleStatements: true,
+              host: 'localhost',
+              user: 'root',
+              password: '1234',
+              database: 'TEST'
+            });
+
+            con.query('SELECT * FROM users' , function(err, rows) {
+              lastidi = rows.length;
+              console.log(lastidi);
+              lastidi += 1;
+              newidi = maxidi - lastidi;
+
+              console.log(newidi);
+              ws.send("newi" + newidi);
+
+            });
+
+
+
 
             break;
 
@@ -97,6 +124,7 @@ serv1.on('connection', function(ws) {
             var pw = content.slice(10);
 
             var con = mysql.createConnection({
+              multipleStatements: true,
               host: 'localhost',
               user: 'root',
               password: '1234',
@@ -132,96 +160,112 @@ serv1.on('connection', function(ws) {
 
       switch (id) {
         case 'refr':
-	  if (conect) { // If already conected
+        	  if (conect) { // If already conected
 
-          var con = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '1234',
-            database: 'TEST'
-          });
-          con.connect(function(err){
-            if (err) throw err;
-            console.log("Connected!");
-          });
-          values = [[identifiant]];
-          con.query('SELECT solde FROM accounts WHERE account=?', [values] , function(err, rows) {
-            if (!err && rows.length !== 0){
-              ws.send('capi' + rows[0].solde);
-            }
-            else {
-              ws.send("cape");
-            }
-            });
+                  var con = mysql.createConnection({
+                    multipleStatements: true,
+                    host: 'localhost',
+                    user: 'root',
+                    password: '1234',
+                    database: 'TEST'
+                  });
+                  con.connect(function(err){
+                    if (err) throw err;
+                    console.log("Connected!");
+                    values = [[identifiant]];
+                  });
 
-            con.end();
-	    };
-            break;
+                  con.query('SELECT solde FROM accounts WHERE account=?', [values] , function(err, rows) {
+                    if (!err && rows.length !== 0){
+                      ws.send('capi' + rows[0].solde);
+                    }
+                    else {
+                      ws.send("cape");
+                    }
+                    });
+
+                    con.end();
+        	          };
+                    break;
 
         case 'vire':
-	  if (conect) { // If already conected
+      	  if (conect) { // If already conected
 
-          var destinataire = content.slice(0, 10);
-          var montant = content.slice(10);
+                var destinataire = content.slice(0, 10);
+                console.log(destinataire);
+                var montant = content.slice(10);
+                var status = false;
+                var montantDestinataire = 0;
+                console.log(montant)
 
-          var con = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '1234',
-            database: 'TEST'
-          });
-          con.connect(function(err){
-            if (err) throw err;
-            console.log("Connected!");
-          });
-          values = [[identifiant]];
-          con.query('SELECT solde FROM solde WHERE account=?', [values] , function(err, rows) {
-            if (!err && rows.length !== 0){
-              if (rows[0].solde > montant) {
-                status = true;
-                var montantAccount = rows[0].solde;
-              }
-              else {
-                status = false;
-              }
-            }
-            else
-              ws.send("coreno");
-            });
+                var con = mysql.createConnection({
+                  multipleStatements: true,
+                  host: 'localhost',
+                  user: 'root',
+                  password: '1234',
+                  database: 'TEST'
+                });
+                con.connect(function(err){
+                  if (err) throw err;
+                  console.log("Connected!");
+                });
+                values = [[identifiant]];
+                con.query('SELECT solde FROM accounts WHERE account=?', [values] , function(err, rows) {
+                  if (!err && rows.length !== 0){
+                    if (rows[0].solde >= montant) {
+                      status = true;
+                      var montantAccount = rows[0].solde;
+                    }
+                    else {
+                      status = false;
+                      ws.send("vivono");
+                    }
+                  }
+                  else{
+                    ws.send("vivono");
+                  }
 
-          if (status) {
-            values = [[destinataire]];
-            con.query('SELECT solde FROM solde WHERE account=?', [values] , function(err, rows) {
-              if (rows.length == 0) {
-                status = false;
-              }
-              else {
-                var montantDestinataire = rows[0].solde;
-              }
-              if (err) throw err;
-              });
-          }
+                    console.log(status);
 
-          if (status) {
-            var reste = montantAccount - montant;
-            solde = reste;
-            values = [[solde,identifiant]];
-            con.query('UPDATE accounts SET solde=? WHERE account=?', [values] , function(err, rows) {
-              if (err) throw err;
-            });
+                    if (status) {
+                      values = [[destinataire]];
+                      con.query('SELECT solde FROM accounts WHERE account=?', [values] , function(err, rows) {
+                        if (err) {
+                          status = false;
+                          ws.send("vivono");
+                        }
+                        else {
+                          montantDestinataire = rows[0].solde;
+                        }
+                        });
+                    }
 
-            var newmontant = montantDestinataire + montant;
-            solde = newmontant
-            values = [[solde,destinataire]];
-            con.query('UPDATE accounts SET solde=? WHERE account=?', [values] , function(err, rows) {
-              if (err) throw err;
-            });
-          }
+                    if (status) {
+                      var reste = montantAccount - montant;
+                      solde = reste;
+                      console.log(solde);
+                      values = [[solde]];
+                      var values2 = [[identifiant]];
+                      con.query('UPDATE accounts SET solde=? WHERE account=?', [values,values2] , function(err, rows) {
+                      });
 
+                      var newmontant = montantDestinataire + montant;
+                      solde = newmontant
+                      console.log(solde);
+                      values = [[solde]];
+                      var values2 = [[destinataire]];
+                      con.query('UPDATE accounts SET solde=? WHERE account=?', [values,values2] , function(err, rows) {
+                      });
+                      ws.send("vicook");
+                    }
+                  else {
+                    ws.send("vivono");
+                  }
 
-          con.end();
-	  };
-          break;
+                  });
+                con.end();
+      	       }
+                break;
 
       };
     });
@@ -234,3 +278,4 @@ serv1.on('connection', function(ws) {
 
 
 server.listen(4445);
+
